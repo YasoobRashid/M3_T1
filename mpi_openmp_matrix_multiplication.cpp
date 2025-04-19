b7a92e2e96999f2a3859829e9ff9c35ff5c3298a
YasoobRashid <yasoobjavaid080@gmail.com>
2025-04-19 15:52:26 +0530

    Create mpi_openmp_matrix_multiplication.cpp

---
+++ mpi_openmp_matrix_multiplication.cpp
@@ -0,0 +1,71 @@
+#include <mpi.h>
+#include <omp.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+const int N = 750;
+
+void generateMatrix(vector<int> &matrix) {
+    for (int i = 0; i < N * N; i++) {
+        matrix[i] = rand() % 10;
+    }
+}
+
+void multiplyChunkOpenMP(const vector<int> &A_chunk, const vector<int> &B, vector<int> &C_chunk, int rowsPerProc) {
+    #pragma omp parallel for
+    for (int i = 0; i < rowsPerProc; i++) {
+        for (int j = 0; j < N; j++) {
+            int sum = 0;
+            for (int k = 0; k < N; k++) {
+                sum += A_chunk[i * N + k] * B[k * N + j];
+            }
+            C_chunk[i * N + j] = sum;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rowsPerProc = N / size;
+
+    vector<int> A, B(N * N), C;
+
+    if (rank == 0) {
+        A.resize(N * N);
+        C.resize(N * N);
+        generateMatrix(A);
+        generateMatrix(B);
+    }
+
+    vector<int> A_chunk(rowsPerProc * N);
+    vector<int> C_chunk(rowsPerProc * N);
+
+    MPI_Bcast(B.data(), N * N, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A.data(), rowsPerProc * N, MPI_INT, A_chunk.data(), rowsPerProc * N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    auto start = high_resolution_clock::now();
+
+    multiplyChunkOpenMP(A_chunk, B, C_chunk, rowsPerProc);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    MPI_Gather(C_chunk.data(), rowsPerProc * N, MPI_INT, C.data(), rowsPerProc * N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        cout << "MPI + OpenMP Execution Time: " << duration.count() << " ms" << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
